070e7951e6cae1a3684362f942611133cbd8f953
louis nwosu <nwachukwuchinanu1@gmail.com>
2021-01-21 10:41:53 +0000

    making a game with python

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    def __init__(self):
+        #screen setting
+        self.width = 1200
+        self.height = 700
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
